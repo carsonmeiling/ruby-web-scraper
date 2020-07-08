a18b1d710c7282ad89f15274e9628ae8dea4038d
Carson Meiling <carmc93@gmail.com>
2020-07-08 10:00:35 -0600

    moved call outside

--- scraper.rb
+++ scraper.rb
@@ -1,36 +1,29 @@
+
 require 'httparty'
-require 'Nokogiri'
+require 'nokogiri'
+
 
 class Scraper
-
-attr_accessor :parse_page
-
+  attr_accessor :parse_page
   def initialize
-    doc = HTTParty.get("https://www.nike.com/w/mens-lifestyle-shoes-13jrmznik1zy7ok")
+    doc = HTTParty.get("http://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3")
     @parse_page ||= Nokogiri::HTML(doc)
   end
-
   def get_names
-    names = item_container.css(".product-msg__info").css(".product-card__titles").css(".product-card__title").children.map { |name| name.text }.compact
+    names = item_container.css(".product-name").css("p").children.map { |name| name.text }.compact
   end
-
-  # def get_price
-  #   price = item_container.css(".product-price").css("span-local").children.map { |price| price.text }.compact
-  # end
-
+  def get_prices
+    price = item_container.css(".product-price").css("span-local").children.map { |price| price.text }.compact
+  end
   private
-
   def item_container
-    parse_page.css(".product-card").css(".product-card__body").css(".product-card__info")
+    parse_page.css(".grid-item-info")
   end
-
-  scraper = Scraper.new
-  names = Scraper.get_names
-  prices = Scraper.get_price
-
-  (0...prices.size).each do |index| 
-    puts "- - - index: #{index + 1} - - -  "
-    puts " Name: #{names [index]}"
-    # puts " Name: #{names [index]} Price: #{prices[index]}"
-  end
+end
+scraper = Scraper.new
+prices = scraper.get_prices
+names = scraper.get_names
+(0...prices.size).each do |index| 
+  puts "- - - index: #{index + 1} - - -  "
+  puts " Name: #{names [index]} Price: #{prices[index]}"
 end
